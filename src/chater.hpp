10a0760b15f1c4def7338a6c691aae90fcf53ec5
Nikolay Bryskin <nbryskin@yandex-team.ru>
2010-06-02 14:19:24 +0400

    added chater class: it replies to user input from stdin

---
+++ src/chater.hpp
@@ -0,0 +1,63 @@
+/*
+ * chater.hpp
+ *
+ *  Created on: Jun 2, 2010
+ *      Author: nbryskin
+ */
+
+#ifndef CHATER_HPP_
+#define CHATER_HPP_
+
+#include <boost/asio.hpp>
+#include <iostream>
+
+#include "common.hpp"
+
+class chater
+{
+public:
+    chater(asio::io_service& io, int fd)
+    : stdin(io, fd)
+    {
+    }
+    void start()
+    {
+        start_waiting_input();
+    }
+
+private:
+    void start_waiting_input()
+    {
+        asio::async_read_until(stdin, input_buffer, '\n', boost::bind(&chater::finished_waiting_input, this, placeholders::error()));
+    }
+
+    void finished_waiting_input(const error_code& ec)
+    {
+        TRACE_ERROR(ec);
+        if (ec)
+            return;
+
+        std::string command;
+        std::istream input(&input_buffer);
+        input >> command;
+        input.get();
+
+        if (command == "dump")
+            dump_action();
+
+        start_waiting_input();
+    }
+
+    void dump_action()
+    {
+        proxy* p = find_proxy();
+
+        p->dump_channels_state();
+    }
+
+    asio::posix::stream_descriptor stdin;
+    asio::streambuf input_buffer;
+    logger log;
+};
+
+#endif /* CHATER_HPP_ */
